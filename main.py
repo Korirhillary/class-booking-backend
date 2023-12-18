61111f2c49fa4c981d3572303a37ac2d78f0ac9d
hillary <korirhillary67@gmail.com>
2023-12-18 17:02:58 +0300

    updated main.py

--- main.py
+++ main.py
@@ -2,10 +2,27 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
+#define a route
+@app.get('/lecturehalls')
 def index():
     return{"Message":"welcome to my first aPi"}
 
-@app.post('/')
+@app.get('/lecturehalls/{lecturehall_id}')
+def sunday():
+    return[]
+
+# create a lecturehall
+@app.post('/lecturehalls')
 def create():
-    return{"Message":"welcome to 2024"}
+    return{"Message":"Happy New year 2024"}
+
+# update Lecturehall
+@app.patch('/lecturehalls/{lecturehall_id}')
+def updated_event(lecturehall_id:int):
+    return{"message":f"{lecturehall_id}created successfully"}
+ 
+ #delete lecturehall
+@app.delete('/lecturehalls/{lecturehall_id}')
+def delete_lecturehall(lecturehall_id:int):
+    return{"message":f"{lecturehall_id} deleted successfully"}
+
